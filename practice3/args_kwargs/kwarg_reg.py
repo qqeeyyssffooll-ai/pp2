d85a63daaf3924ef024e4ba4a65c4031bcfbbfbe
qqeeyyssffooll-ai <qqeeyyssffooll@gmail.com>
2026-01-30 18:44:41 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ practice3/args_kwargs/kwarg_reg.py
@@ -0,0 +1,7 @@
+def my_function(username, **details):
+  print("Username:", username)
+  print("Additional details:")
+  for key, value in details.items():
+    print(" ", key + ":", value)
+
+my_function("emil123", age = 25, city = "Oslo", hobby = "coding")
